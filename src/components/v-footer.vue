893c2f644d5e8f9a02a105bc11e7c54048844f23
vitaliy.koltochihin <vitaliy.koltochihin@eltex.loc>
2024-03-27 10:41:40 +0700

    task

--- src/components/v-footer.vue
+++ src/components/v-footer.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="v-footer">
-
+    <div class="footer-contact-block">
+        <img src="../images/icons/map.png" class="contact-icon footer-contact-item">
+        <div class="footer-contact-item"> Искитим, ул. Комсомольская, 44 </div>
+        <br>
+        <a href="https://vk.com/klinika_zdoroviya"><img src="../images/icons/VK_logo.png" class="contact-icon social footer-contact-item"></a>
+        <a href="https://www.instagram.com/klinika_zdoroviya_iskitim/?hl=es"><img src="../images/icons/Inst_logo.png" class="contact-icon social footer-contact-item"></a>
+    </div>
+    <div class="copyright">©Клиника здоровье, 2024 г.</div>
   </div>
 </template>
 
@@ -15,7 +22,25 @@ export default {
 <style>
 .v-footer {
     margin-top: 40px;
-    height: 80px;
     background-color: #b9ecad;
+}
+
+.footer-contact-block {
+  text-align: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.footer-contact-item {
+  display: inline;
+  vertical-align: middle;
+  font-size: 16px;
+}
+
+.copyright {
+  text-align: center;
+  font-size: 13px;
+  padding-bottom: 10px;
+  opacity: 0.7;
 }
 </style>
